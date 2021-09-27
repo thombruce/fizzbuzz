d2f8851386f27b7aa29fafa64a7eeb3e8835970e
Thom Bruce <thom@thombruce.com>
2021-09-27 20:26:12 +0100

    Add docs site

---
+++ packages/docs/layouts/default.vue
@@ -0,0 +1,26 @@
+<template lang='pug'>
+.h-screen.flex.flex-col
+  header.flex-none.navbar.bg-neutral.text-neutral-content
+    .navbar-start
+      h1.text-lg.font-bold
+        NuxtLink(to='/') Thom Bruce
+    .navbar-center
+      span FizzBuzz
+    .navbar-end
+      TntUIThemeToggle
+  main.flex-1.overflow-auto
+    Nuxt
+  footer.flex-none.footer.footer-center.bg-neutral.text-neutral-content
+    div
+      p © Thom Bruce 2021
+</template>
+
+<script>
+import { themeChange } from 'theme-change'
+
+export default {
+  mounted () {
+    themeChange(false)
+  }
+}
+</script>
