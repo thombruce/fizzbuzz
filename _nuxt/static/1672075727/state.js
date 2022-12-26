fa69159fa2b92f6e50257aa7cbb54aa4e922fb55
thombruce <thombruce@users.noreply.github.com>
2022-12-26 17:28:50 +0000

    deploy: 5871b1e1ea34c0c973a3e9547cc827542e2711eb

---
+++ _nuxt/static/1672075727/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Ffizzbuzz\u002F_nuxt\u002Fstatic\u002F1672075727",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002Ffizzbuzz\u002F",assetsPath:"\u002Ffizzbuzz\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"1c0bb76f"}}}}(null));
